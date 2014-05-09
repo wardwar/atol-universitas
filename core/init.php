51babd5983817d2fbf3f49094e3527ff799d8907
Wildan Angga Rahman <wildan.aga@gmail.com>
2014-05-09 23:00:03 +0700

    isi init.php

---
+++ core/init.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+$GLOBALS['config'] = array(
+	'mysql' => array(
+		'host' => '127.0.0.1',
+		'username' => 'root',
+		'password' => '',
+		'db' => 'universitas'
+	),
+	'remember' => array(
+		'cookie_name' => 'hash',
+		'cookie_expire' => 604800
+	),
+	'session' => array(
+		'session_name' => 'user'
+		)
+);
+
+spl_autoload_register(function($class){
+	require_once 'classes/' . $class . '.php';
+});
+
+require_once 'function/sanitize.php';
